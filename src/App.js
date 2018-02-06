81de4cc527c7fa327dfd2de622a201a61502542f
vicdata <vghaya@gmail.com>
2018-02-06 23:37:08 +0100

    Paginated Fetch

--- src/App.js
+++ src/App.js
@@ -5,8 +5,9 @@ const DEFAULT_QUERY = 'redux';
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page='
 
-const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}&${PARAM_PAGE}`;
 console.log(url)
 
 /*
@@ -60,10 +61,14 @@ class App extends Component {
     this.onClickMe = this.onClickMe.bind(this)
   }
   setSearchTopStories(result) {
-    this.setState({result})
+    const { hits, page } = result
+    const oldHits = page !== 0 ? this.state.result.hits : []
+    const updatedHits = [...oldHits,...hits]
+
+    this.setState({result: { hits:updatedHits, page }})
   }
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(e => e);
@@ -95,6 +100,7 @@ class App extends Component {
     render() {
     const vicdataText = 'HackerNews VD'
     const { searchTerm, result } = this.state
+    const page = (result && result.page) || 0
     if (!result) {return null}
     return (
       <div className="page">
@@ -105,6 +111,9 @@ class App extends Component {
         { result &&
         <Table list={result.hits} onDismiss={this.onDismiss} />
         }
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>More</Button>
+        </div>
         {/*<button onClick={this.onClickMe} type="button">Click Me</button>*/}
       </div>
 
