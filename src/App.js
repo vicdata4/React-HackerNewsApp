99aa283c288c911da45e40dcab355f220b67eba3
vicdata <vghaya@gmail.com>
2018-02-06 21:37:35 +0100

    Fetching data from Hacker News API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+console.log(url)
+
+/*
 const list = [
 {
   title: 'React',
@@ -20,7 +28,6 @@ const list = [
   objectID: 1
 }
 ];
-const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase())
 
 const user = {firstname: 'Robin', lastname: 'Wieruch'}
 console.log(user)
@@ -34,22 +41,42 @@ const [
   userThree
 ] = users
 console.log(userOne + '>>>' + userTwo + '>>>' + userThree)
+*/
+const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase())
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: list,
-      searchTerm: ''
+      result: null,
+      searchTerm: DEFAULT_QUERY
     };
+    this.setSearchTopStories = this.setSearchTopStories.bind(this)
+    this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
     this.onSearchChange = this.onSearchChange.bind(this);
     this.onDismiss = this.onDismiss.bind(this)
     this.onClickMe = this.onClickMe.bind(this);
   }
+  setSearchTopStories(result) {
+    this.setState({result})
+  }
+  fetchSearchTopStories(searchTerm) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+      .then(response => response.json())
+      .then(result => this.setSearchTopStories(result))
+      .catch(e => e);
+  }
+  componentDidMount() {
+    const { searchTerm } = this.state;
+    this.fetchSearchTopStories(searchTerm);
+  }
   onDismiss(id) {
     const isNotId = item => item.objectID !== id
-    const updatedList = this.state.list.filter(isNotId)
-    this.setState({ list: updatedList })
+    const updatedHits = this.state.result.hits.filter(isNotId)
+    this.setState({
+      /*result: Object.assign({}, this.state.result, { hits: updatedHits })*/
+      result: { ...this.state.result, hits: updatedHits }
+    })
   }
   onClickMe() {
     console.log(this)
@@ -60,14 +87,15 @@ class App extends Component {
 
     render() {
     const vicdataText = 'Vicdata4'
-    const { searchTerm, list } = this.state
+    const { searchTerm, result } = this.state
+    if (!result) {return null}
     return (
       <div className="page">
         <div className="interactions">
           <h2>{vicdataText}</h2>
           <Search value={searchTerm} onChange={this.onSearchChange}>Search:</Search>
         </div>
-      <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
+      <Table list={result.hits} pattern={searchTerm} onDismiss={this.onDismiss} />
         {/*<button onClick={this.onClickMe} type="button">Click Me</button>*/}
       </div>
 
