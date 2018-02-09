30f2cab4e941069841012480e217b27532adc7ef
vicdata <vghaya@gmail.com>
2018-02-09 21:29:31 +0100

    Ref a DOM element (without this object)

--- src/Search/search.js
+++ src/Search/search.js
@@ -1,14 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-export const Search = ({ value, onChange, children, onSubmit }) => {
-    return (
-      <form onSubmit={onSubmit}>
-      <input type="text" value={value} onChange={onChange}></input>
+export const Search = ({
+  value,
+  onChange,
+  onSubmit,
+  children
+}) => {
+  let input;
+  return (
+    <form onSubmit={onSubmit}>
+      <input type="text" value={value} onChange={onChange} ref={(node) => { input = node; }}></input>
       <button type="submit">{children}</button>
-      </form>
-    )
+    </form>
+  )
 }
+
 
 Search.propTypes = {
   value: PropTypes.string,
